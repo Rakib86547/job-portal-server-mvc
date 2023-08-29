7297a9bfb8a7ca59a572ba555f998ce3e76fbc4c
MD Rakib Khan <contact.antor.bd@gmail.com>
2023-08-29 19:00:30 +0600

    search function

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -2,12 +2,13 @@ const { getSearchJobsService } = require("../services/search.service");
 
 exports.getSearchJobs = async (req, res) => {
     try {
-        const { title, location } = req.query;
-        const result = await getSearchJobsService(title, location);
-        res.status(200).send({
-            status: 'Success',
-            data: result
-        })
+        const { title, location } = req.query;    
+            const result = await getSearchJobsService(title, location);
+            res.status(200).send({
+                status: 'Success',
+                total: result.length,
+                data: result
+            })        
     } catch (error) {
         res.status(400).send({
             status: 'Fail',
--- services/search.service.js
+++ services/search.service.js
@@ -1,21 +1,32 @@
 const Jobs = require("../models/Jobs")
 
 exports.getSearchJobsService = async (title, location) => {
-    console.log(title, location)
-    if (title !== undefined) {
+    // if (title !== undefined) {
+    //     const result = await Jobs.find({
+    //         "$or": [
+    //             { position: { $regex: title } },
+    //         ]
+    //     });
+    //     return result
+    // }
+    // else if (location !== undefined) {
+    //     const result = await Jobs.find({
+    //         "$or": [
+    //             { location: { $regex: location } },
+    //         ]
+    //     });
+    //     return result
+    // }
+    if (title !== undefined ) {
         const result = await Jobs.find({
-            "$or": [
-                { position: { $regex: title } },
-            ]
+            position: { $regex: new RegExp(`^${title}`, 'i') }
         });
         return result
     }
-    else if (location !== undefined) {
+    else if ( location !== undefined) {
         const result = await Jobs.find({
-            "$or": [
-                { location: { $regex: location } },
-            ]
+            location: { $regex: new RegExp(`^${location}`, 'i') }
         });
         return result
-    }
+    }   
 }
