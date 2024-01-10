ff542d7cf5f65c0a86f12be2ddafcb653d3cd447
MD Rakib Khan <contact.antor.bd@gmail.com>
2024-01-11 00:59:46 +0600

    commit

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 // -----Middleware----
 app.use(express.json());
 app.use(cors());
-// app.use(errorHandler)
 const jobsRoute = require('./routes/v1/jobs.routes');
 const categoryRoute = require('./routes/v1/category.route');
 const userRoute = require('./routes/v1/user.route');
